2633609df022d6ca047dbeed48cf23c06c02a257
Sam Gwilym <gwilym@me.com>
2022-04-05 09:58:56 +0200

    Add a script to write posts into a replica

---
+++ my_script.ts
@@ -0,0 +1,52 @@
+import * as Earthstar from "https://deno.land/x/earthstar@8.2.4/mod.ts";
+import { join } from "https://deno.land/std@0.133.0/path/mod.ts";
+
+// Import the identity we made!
+import keypair from "./identity.json" assert { type: "json" };
+
+// =========================================================================
+
+// Setting up a Replica
+
+// 1. Come up with a cool share name.
+const MY_SHARE = "+gwilzone.q7uhabux";
+
+// 2. Let's make an in-memory replica driver.
+const driver = new Earthstar.ReplicaDriverMemory(MY_SHARE);
+
+// 3. Let's use that to build our replica!
+const replica = new Earthstar.Replica(
+  MY_SHARE,
+  Earthstar.FormatValidatorEs4,
+  driver,
+);
+
+// =========================================================================
+
+// Writing our blog posts into the replica.
+
+// 1. Keep the path to our paths post in a handy variable.
+const POSTS_PATH = "./posts";
+
+// 2. Iterate over the contents of the posts directory.
+for await (const entry of Deno.readDir(POSTS_PATH)) {
+  const filePath = join(POSTS_PATH, entry.name);
+
+  // 3. Read the contents of our file.
+  const fileContents = await Deno.readTextFile(filePath);
+
+  // 4. Write them into our replica!
+  await replica.set(keypair, {
+    path: `/posts/${entry.name}`,
+    content: fileContents,
+    format: "es.4",
+  });
+}
+
+// =========================================================================
+
+// Let's see if it worked!
+
+const allDocs = await replica.getLatestDocs();
+
+console.log(allDocs);
